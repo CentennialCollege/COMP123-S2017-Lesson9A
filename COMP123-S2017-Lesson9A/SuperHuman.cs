55147ccf4a1816042d3209c1c0f618a8f1c0e2bf
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-11 15:41:42 -0400

    Add SuperHuman sub class

---
+++ COMP123-S2017-Lesson9A/SuperHuman.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: July 11, 2017
+ * Description: This is the SuperHuman sub class.
+ * Version: 0.1 - Created the SuperHuman sub class
+ */
+
+namespace COMP123_S2017_Lesson9A
+{
+    /// <summary>
+    /// This is the SuperHuman sub class.
+    /// It inherits from the Human Superclass.
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        // PRIVATE FIELDS
+
+        // PUBLIC PROPERTIES
+
+        // CONSTRUCTORS
+        public SuperHuman(string name)
+            :base(name)
+        {
+
+        }
+
+        // PUBLIC METHODS
+    }
+}
