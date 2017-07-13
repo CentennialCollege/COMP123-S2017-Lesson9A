247683ef5c592ca3c91870aeb19706ef2a7782f6
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-13 12:57:58 -0400

    Used the ToString method of the SuperHuman class in the Driver class

--- COMP123-S2017-Lesson9A/Program.cs
+++ COMP123-S2017-Lesson9A/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
  * Name: Tom Tsiliopoulos
  * Date: July 11, 2017
  * Description: This is the driver class for my project
- * Version: 0.2 - Added a power to the superHuman object and displayed it
+ * Version: 0.3 - Used the ToString Method of the SuperHuman class
  */
 
 namespace COMP123_S2017_Lesson9A
@@ -19,7 +19,10 @@ namespace COMP123_S2017_Lesson9A
         {
             SuperHuman superHuman = new SuperHuman("Super Dude");
             superHuman.Powers.Add(new Power("Teleportation",50));
-            superHuman.DisplayPowers();
+            superHuman.Powers.Add(new Power("Phasing", 40));
+            superHuman.Powers.Add(new Power("Invisibility", 30));
+            Console.WriteLine(superHuman.ToString());
+
         }
     }
 }
