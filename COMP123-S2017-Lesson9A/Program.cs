4455aa173751be9ab5024f3633d55eeb16342b65
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-13 13:33:19 -0400

    Implemented the SuperHero class in the Driver class

--- COMP123-S2017-Lesson9A/Program.cs
+++ COMP123-S2017-Lesson9A/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
  * Name: Tom Tsiliopoulos
  * Date: July 11, 2017
  * Description: This is the driver class for my project
- * Version: 0.3 - Used the ToString Method of the SuperHuman class
+ * Version: 0.4 - Implemented the SuperHero class
  */
 
 namespace COMP123_S2017_Lesson9A
@@ -17,11 +17,11 @@ namespace COMP123_S2017_Lesson9A
     {
         static void Main(string[] args)
         {
-            SuperHuman superHuman = new SuperHuman("Super Dude");
-            superHuman.Powers.Add(new Power("Teleportation",50));
-            superHuman.Powers.Add(new Power("Phasing", 40));
-            superHuman.Powers.Add(new Power("Invisibility", 30));
-            Console.WriteLine(superHuman.ToString());
+            SuperHero superHero = new SuperHero("SuperHero Dude", 20);
+            superHero.Powers.Add(new Power("Teleportation",50));
+            superHero.Powers.Add(new Power("Phasing", 40));
+            superHero.Powers.Add(new Power("Invisibility", 30));
+            Console.WriteLine(superHero.ToString());
 
         }
     }
--- COMP123-S2017-Lesson9A/SuperHero.cs
+++ COMP123-S2017-Lesson9A/SuperHero.cs
@@ -8,7 +8,7 @@ using System.Text;
  * Date: July 13, 2017
  * Description: This is the SuperHero class which inherits from the SuperHuman class
  * and implements the IHasKarma Interface
- * Version: 0.1 - Created the SuperHero class
+ * Version: 0.2 - Modified Constructor to take Karma Value
  */
 
 namespace COMP123_S2017_Lesson9A
@@ -43,10 +43,10 @@ namespace COMP123_S2017_Lesson9A
         /// It takes one argument - name (string)
         /// </summary>
         /// <param name="name"></param>
-        public SuperHero(string name)
+        public SuperHero(string name, int karma)
             :base(name)
         {
-
+            this.Karma = karma
         }
 
         // PRIVATE METHODS
